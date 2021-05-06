6c41c36301a8b887c8babdfc82a2ddfc8d13a980
figo5530 <figooooo0221@gmail.com>
2021-05-06 16:08:16 +0800

    set up relationship and seed some data

---
+++ app/serializers/watch_list_serializer.rb
@@ -0,0 +1,3 @@
+class WatchListSerializer < ActiveModel::Serializer
+  attributes :id, :name
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+watch_lists = WatchList.create([{name: "2021", movies: [
+    Movie.create(title: 'The Lord of the Rings: The Fellowship of the Ring'), 
+    Movie.create(title: 'The Lord of the Rings: The Two Towers'), 
+    Movie.create(title: 'The Lord of the Rings: The Return of the King')]}, 
+    {name: "2020", movies: [
+        Movie.create(title: 'Harry Potter and the Sorcerer\'s Stone'), 
+        Movie.create(title: 'Harry Potter and the Chamber of Secrets'), 
+        Movie.create(title: 'Harry Potter and the Prisoner of Azkaban'), 
+        Movie.create(title: 'Harry Potter and the Goblet of Fire'), 
+        Movie.create(title: 'Harry Potter and the Order of the Phoenix'), 
+        Movie.create(title: 'Harry Potter and the Half-Blood Prince'), 
+        Movie.create(title: 'Harry Potter and the Deathly Hallows')]}])
