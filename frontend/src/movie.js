843c1bc33889865485e55387ee3587f422fd2edb
figo5530 <figooooo0221@gmail.com>
2021-05-07 01:31:28 +0800

    create delete button while generating li tag

--- frontend/src/movie.js
+++ frontend/src/movie.js
@@ -4,7 +4,16 @@ function appendMovies(movies, ele) {
     ele.append(ul)
     for (const movie of movies) {
         const li = document.createElement("li")
+        const movieDelete = createDeleteButton()
         li.innerText = movie.title
         ul.append(li)
+        ul.append(movieDelete)
     }
+}
+
+function createDeleteButton() {
+    const movieDelete = document.createElement("button")
+    movieDelete.innerText = "Delete"
+    movieDelete.className = "genric-btn success radius"
+    return movieDelete
 }
