b8a38b850202a45f6b6528d64ad4e429a3331038
figo5530 <figooooo0221@gmail.com>
2021-05-07 02:12:59 +0800

    finish createDeleteButton method and deleteMovie and appendMovies method

--- frontend/src/movie.js
+++ frontend/src/movie.js
@@ -4,16 +4,30 @@ function appendMovies(movies, ele) {
     ele.append(ul)
     for (const movie of movies) {
         const li = document.createElement("li")
-        const movieDelete = createDeleteButton()
         li.innerText = movie.title
+        const movieDelete = createDeleteButton()
+        movieDelete.addEventListener('click', function(e) {
+            deleteMovie(movie.id, li)
+        })
+        li.append(movieDelete)
         ul.append(li)
-        ul.append(movieDelete)
     }
 }
 
 function createDeleteButton() {
+    const div = document.createElement("div")
+    div.className = "button-group-area mt-40"
     const movieDelete = document.createElement("button")
     movieDelete.innerText = "Delete"
     movieDelete.className = "genric-btn success radius"
-    return movieDelete
+    div.append(movieDelete)
+    return div
+}
+
+function deleteMovie(movieId, ele) {
+    fetch(`http://localhost:3000/movies/${movieId}`, {
+        method: "DELETE"}).then(resp => resp.json())
+        .then(m => {
+            ele.remove()
+        })
 }
