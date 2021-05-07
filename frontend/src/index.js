a510484a1656eb965feaca5cef948686c6a2ce6b
figo5530 <figooooo0221@gmail.com>
2021-05-07 14:41:24 +0800

    rewrite watchlist.js file with OOJS

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,4 @@
-fetchWatchList()
-form.addEventListener('submit', postList)
+WatchList.fetchWatchList()
+
+const form = document.getElementById("form")
+form.addEventListener('submit', WatchList.postList)
--- frontend/src/watchlist.js
+++ frontend/src/watchlist.js
@@ -1,59 +1,73 @@
-function fetchWatchList() {
-    fetch("http://localhost:3000/watch_lists").then(resp => resp.json()).then(lists => {
-        appendLists(lists)
-    })
-}
+class WatchList {
+    constructor(watchlist) {
+        this.name = watchlist.name
+        this.id = watchlist.id
+        this.movies = watchlist.movies
+    }
 
-function appendLists(lists) {
-    const liDiv = document.getElementById("listDiv")
-    for (const list of lists) {
+    appendList() {
+        const liDiv = document.getElementById("listDiv")
         const div = document.createElement("div")
         const innerDiv = document.createElement("div")
         const tag = document.createElement("h3")
         innerDiv.append(tag)
-        setCss(div, innerDiv, tag)
-        tag.innerText = list.name
-        tag.addEventListener('click', (e) => renderListShowPage(list))
-        appendMovies(list.movies, innerDiv)
+        WatchList.setCss(div, innerDiv, tag)
+        tag.innerText = this.name
+        tag.addEventListener('click', this.renderListShowPage.bind(this))
+        appendMovies(this.movies, innerDiv)
         div.append(innerDiv)
         liDiv.append(div) 
     }
-}
 
-function setCss(div, innerDiv, tag) {
-    div.className = "section-top-border"
-    innerDiv.className = "col-lg-4 col-sm-6 mt-sm-30 typo-sec"
-    tag.className = "mb-20 title_color"
-}
-
-const form = document.getElementById("form")
-
-function postList(e) {
-    e.preventDefault()
-    const userInput = e.target.children[0].children[0].value
-    e.target.reset()
-    const option = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify({watchlist: {name: userInput}})
+    renderListShowPage() {
+        const liContainer = document.getElementById("container")
+        liContainer.children[1].innerHTML = ""
+        liContainer.children[0].remove()
+        appendMovieForm()
+        this.appendList()
     }
-    fetch("http://localhost:3000/watch_lists", option).then(resp => resp.json()).then(list => {
-        const arr = []
-        arr.push(list)
-        appendLists(arr)
-    })
+
+    static fetchWatchList() {
+        fetch("http://localhost:3000/watch_lists").then(resp => resp.json())
+        .then(this.appendLists)
+    }
+    
+    static appendLists(lists) {
+        for (const list of lists) {
+            let watchList = new WatchList(list)
+            watchList.appendList()
+        }
+    }
+
+    static postList(e) {
+        e.preventDefault()
+        const userInput = e.target.children[0].children[0].value
+        e.target.reset()
+        const option = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({watchlist: {name: userInput}})
+        }
+        fetch("http://localhost:3000/watch_lists", option).then(resp => resp.json()).then(list => {
+            let watchList = new WatchList(list)
+            watchList.appendList()
+        })
+    }
+
+    static setCss(div, innerDiv, tag) {
+        div.className = "section-top-border"
+        innerDiv.className = "col-lg-4 col-sm-6 mt-sm-30 typo-sec"
+        tag.className = "mb-20 title_color"
+    }
 }
 
-function renderListShowPage(list) {
-    const liContainer = document.getElementById("container")
-    liContainer.children[1].innerHTML = ""
-    liContainer.children[0].remove()
-    appendMovieForm()
-    const arr = []
-    arr.push(list)
-    appendLists(arr)
-}
+
+
+
+
+
+
 
